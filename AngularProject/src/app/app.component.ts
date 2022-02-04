26ba26ee0443b1bc1deb6b6930b9a304ac1088b0
bhormilind-dev <bhormilind.dev@gmail.com>
2022-02-03 20:31:38 -0500

    Observable Praactide with async await

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'AngularProject';
+
+
+  ngOnInit(): void {
+    this.printMessage();
+  }
+
+  private async printMessage(): Promise<void> {
+    const result = await this.message$().toPromise();    
+
+    const hello = await this.sayHello$().toPromise();
+    console.log(`result : ${result}`);
+    console.log(`sayHello : ${hello}`);
+  }
+
+  private message$(): Observable<string> {
+    return new Observable<string>(ob => {
+      ob.next('Hello');
+      ob.complete();
+    });
+  }
+
+  private sayHello$(): Observable<string> {
+    return of('Hello User');
+  }
+
 }
